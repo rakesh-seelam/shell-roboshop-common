301edbad2f1fd94fcf3bf791dfe0f9ab91ce8f7f
rakesh <rakeshgautam602@gmail.com>
2026-06-18 12:40:43 +0530

    action

---
+++ catalogue.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+APP_NAME=catalogue
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading products"
+else
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N"
+fi
+
+print_total_time
--- common.sh
+++ common.sh
@@ -30,6 +30,69 @@ VALIDATE(){
     fi
 }
 
+nodejs_setup(){
+    dnf list installed | grep nodejs &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        dnf module disable nodejs -y &>>$LOG_FILE
+        VALIDATE $? "Disabling nodejs"
+
+        dnf module enable nodejs:20 -y &>>$LOG_FILE
+        VALIDATE $? "Enabling nodejs-20"
+
+        dnf install nodejs -y &>>$LOG_FILE
+        VALIDATE $? "Installing nodejs"
+    else 
+    echo -e "nodejs is already installed $Y SKIPPING $N"
+    fi
+}
+
+app_setup(){
+    #Creating system user
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+        VALIDATE $? "Creating System User"
+    else
+    echo -e "Roboshop User already exists $Y SKIPPING $N"
+    fi
+
+    #Creating app
+    mkdir -p /app 
+    VALIDATE $? "Creating temporary directory"
+
+    curl -o /tmp/$APP_NAME.zip https://roboshop-artifacts.s3.amazonaws.com/$APP_NAME-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $APP_NAME Code"
+
+    cd /app
+    VALIDATE $? "Moving to App directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$APP_NAME.zip &>>$LOG_FILE
+    VALIDATE $? "Unzipping $APP_NAME code"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Installing Dependencies"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$APP_NAME.service /etc/systemd/system/$APP_NAME.service
+    VALIDATE $? "Copying catalagoue service"
+
+    systemctl daemon-reload
+    VALIDATE $? "Reloading Daemon"
+
+    systemctl enable $APP_NAME &>>$LOG_FILE
+    systemctl start $APP_NAME
+    VALIDATE $? "Enabling and Starting $APP_NAME"
+}
+
+app_restart(){
+    systemctl restart $APP_NAME
+    VALIDATE $? "Restarting $APP_NAME"
+}
+
 print_total_time(){
     SCRIPT_END_TIME=$(date +%s) 
     TOTAL_TIME=$((SCRIPT_END_TIME-SCRIPT_START_TIME))
